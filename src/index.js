05de73a7d2bcd7dd5cdc5ac5e50599883aff81f7
Anton Alekseev <leha050598@gmail.com>
2022-02-23 20:36:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  bracketsConfig.forEach((e) => {
+    arr.push(e.join(""));
+  });
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (str.indexOf(arr[j]) > -1) {
+        str = str.replace(arr[j], "");
+        i--;
+      }
+    }
+  }
+  return str.length == 0;
+};
